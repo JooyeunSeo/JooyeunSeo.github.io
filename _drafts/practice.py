51c4ef55cba6448abf43f1f539e0edfd6d04117a
seo <seojy2263@gmail.com>
2025-04-02 19:44:05 +0900

    Add leetcode post

--- _drafts/practice.py
+++ _drafts/practice.py
@@ -1,6 +1,15 @@
 import datetime as dt   
 import os
 
+##########################  OS별 맞춤  ##########################
+def clear_screen(self):
+    if os.name == 'nt':         # Windows
+        os.system('cls')
+    elif os.name == 'posix':    # macOS or Linux
+        os.system('clear')
+    else:
+        pass                    # pass in different environments
+################################################################
 
 #######################################
 def leetcode_post_title():
@@ -12,7 +21,6 @@ def leetcode_post_title():
     title_form = date + "-" + title
     print(title_form)
 
-
 def post_title():
     now = dt.datetime.now()
     date = str(now).split(" ")[0]
@@ -21,35 +29,24 @@ def post_title():
     title_form = date + "-" + title
     print(title_form)
 
-
 def new_window_link():
     link = input("link: ")
     text = input("text: ")
     print(f'<a href="{link}" target="_blank">{text}</a>')
 
-
 def highlight_tag():
     highlight_text = input("highlight text: ")
     print(f"<mark>{highlight_text}</mark>")
-#--------------------------------------------#
+
+################################
 leetcode_post_title()
-#--------------------------------------------#
-##########################  OS별 맞춤  ##########################
-def clear_screen(self):
-    if os.name == 'nt':         # Windows
-        os.system('cls')
-    elif os.name == 'posix':    # macOS or Linux
-        os.system('clear')
-    else:
-        pass                    # pass in different environments
-################################################################
+#------------------------------#
+class Solution:
+    def leetcodetest(self):
+        return "leetcode test"
+#------------------------------#
 
-#--------------------------------------------------------------#
-# class Solution:
-    
-
-
-# #---------------#
-# test = Solution()
-# testcase = test.reverseBits(43261596)
-# print(testcase)
+#---------------#
+test = Solution()
+testcase = test.leetcodetest()
+print(testcase)
